832bfc33758bf44da94fe4bac0eb6811d017dc75
Tomoaki-bunsan <iamtomoaki12295@gmail.com>
2020-05-26 10:27:24 +0900

    use RPC

---
+++ New Unity Project/Assets/Network/Scripts/CameraControll.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CameraControll : MonoBehaviour {
+ 
+    private GameObject mainCamera;      //メインカメラ格納用
+    private GameObject subCamera;       //サブカメラ格納用 
+    private GameObject playerObject;            //回転の中心となるプレイヤー格納用
+    public float rotateSpeed = 2.0f;            //回転の速さ
+ 
+ 
+    //呼び出し時に実行される関数
+    void Start () {
+        //メインカメラとサブカメラをそれぞれ取得
+        mainCamera = GameObject.Find("MainCamera");
+        subCamera = GameObject.Find("SubCamera");
+        playerObject = GameObject.Find("unitychan");
+ 
+        //サブカメラを非アクティブにする
+        subCamera.SetActive(false); 
+	}
+	
+ 
+	//単位時間ごとに実行される関数
+	void Update () {
+		//shiftキーが押されている間、サブカメラをアクティブにする
+        if(Input.GetKey("left shift")){
+            //サブカメラをアクティブに設定
+            mainCamera.SetActive(false);
+            subCamera.SetActive(true);
+        }
+        else{
+            //メインカメラをアクティブに設定
+            subCamera.SetActive(false);
+            mainCamera.SetActive(true);
+        }
+        //rotateCameraの呼び出し
+        rotateCamera();
+	}
+
+    private void rotateCamera()
+    {
+        if (Input.GetMouseButton(0)) {
+            //Vector3でX,Y方向の回転の度合いを定義
+            Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed,Input.GetAxis("Mouse Y") * rotateSpeed, 0);
+ 
+            //transform.RotateAround()をしようしてメインカメラを回転させる
+            mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
+            mainCamera.transform.RotateAround(playerObject.transform.position, transform.right, angle.y);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Network/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Mirror;
+using UnityEngine.UI; 
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Network.Scenes.NewScene
+{
+
+    public class Player : NetworkBehaviour
+    {   
+        private bool m_Jump;
+        // 歩行速度（メートル/秒）
+        [SerializeField] float m_WalkSpeed = 1.5f;
+
+        // 各種コンポーネントの参照
+        CharacterController m_CharacterController;
+        Animator m_Animator;
+        
+        // チャットのメッセージ履歴を表示するText
+        Text m_ChatHistory;
+        // メッセージの入力欄
+        InputField m_ChatInputField;
+
+
+
+        void Start()
+        {
+            // 各種コンポーネントの参照を取得する
+            m_CharacterController = GetComponent<CharacterController>();
+            m_Animator = GetComponent<Animator>();
+
+            // メッセージ履歴を表示するTextを検索して取得
+            m_ChatHistory = GameObject.Find("ChatHistory").GetComponent<Text>();
+
+        }
+    
+        // ローカルプレイヤーが初期化される際に呼ばれる。
+        public override void OnStartLocalPlayer()
+        {
+            // メッセージ入力欄のInputFieldを検索して取得
+            m_ChatInputField = GameObject.Find("ChatInputField").GetComponent<InputField>();
+        }
+
+        void Update()
+        {
+
+            
+            // ローカルプレイヤー以外では何もしない
+            if (!isLocalPlayer)
+                return;
+
+            // 移動量(メートル/秒)
+            Vector3 motion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_WalkSpeed;
+
+            // 左シフトキーでダッシュ（2倍速）
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                motion *= 2f;
+            }
+
+            if (!m_Jump)
+            {
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+
+            // CharacterControllerで移動する
+            m_CharacterController.Move(motion * Time.deltaTime);
+
+            // 移動先を向く
+            transform.LookAt(transform.position + motion);
+
+            // Animatorに「Speed」というパラメーター名で移動速度を渡す
+            m_Animator.SetFloat("Speed", motion.magnitude);
+
+            // Enterキーが押されて
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                // 文字列が入力されていたら
+                if (m_ChatInputField.text.Length > 0)
+                {
+                    // Commandを使って入力された文字列をサーバーへ送信
+                    CmdPost(m_ChatInputField.text);
+
+                    // 入力欄を空にする
+                    m_ChatInputField.text = "";
+                }
+            }
+        }
+        // 文字列をサーバーに送信する。
+        [Command]
+        void CmdPost(string text)
+        {
+            RpcPost(text);
+        }
+
+        // ClientRpcは各クライアントで実行される。
+        // チャット履歴表示欄に文字列を追加する。
+        [ClientRpc]
+        void RpcPost(string text)
+        {
+            m_ChatHistory.text += text + System.Environment.NewLine;
+        }
+    }
+}
